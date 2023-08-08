7f5750916abf8d58373543d413db1a33039c1b5c
Nick Claunch <niclau@microsoft.com>
2023-08-08 11:00:04 -0500

    Adding WUfB remediation.

---
+++ Remediations/WindowsUpdateConflicts-Detection.ps1
@@ -0,0 +1,36 @@
+﻿$keys = @()
+
+#Windows Update - AutoUpdate
+$keys+= New-Object -TypeName psobject -Property @{
+    RegistryPath = 'Registry::HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU\'
+    Entries = @('UseUpdateClassPolicySource','UseWUServer','AUOptions','AllowAutoUpdate','NoAutoUpdate')
+}
+
+#Windows Update
+$keys+= New-Object -TypeName psobject -Property @{
+    RegistryPath = 'Registry::HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate'
+    Entries = @('DeferFeatureUpdates','DeferFeatureUpdatesPeriodInDays','PauseFeatureUpdatesStartTime','DeferQualityUpdates','DeferQualityUpdatesPeriodInDays','PauseQualityUpdatesStartTime','ScheduledInstallDay','ScheduledInstallTime','ExcludeWUDriversInQualityUpdate','BranchReadinessLevel','DoNotConnectToWindowsUpdateInternetLocations','DisableWindowsUpdateAccess','SetPolicyDrivenUpdateSourceForDriverUpdates','SetPolicyDrivenUpdateSourceForQualityUpdates','SetPolicyDrivenUpdateSourceForFeatureUpdates','WUServer')
+}
+
+foreach ($key in $keys)
+{
+    try
+    {
+        $value = Get-ItemProperty -Path $key.RegistryPath -ErrorAction Stop
+        Write-Host ('[{0}]: Registry Key exists. Checking for individual values' -f $key.RegistryPath)
+
+        foreach ($entry in $key.Entries)
+        {
+
+            if (-not [string]::IsNullOrWhiteSpace($value.$entry))
+            {
+                Write-Host ('[{0}]: Found conflicting value [{1}]' -f $key.RegistryPath, $entry)
+                exit 1
+            }
+        }
+    }
+    catch
+    {
+        Write-Host ('[{0}]: Registry Key does not exist. No action needed' -f $key.RegistryPath)
+    }
+}
---
+++ Remediations/WindowsUpdateConflicts-Remediation.ps1
@@ -0,0 +1,40 @@
+﻿$keys = @()
+#Windows Update - AutoUpdate
+$keys+= New-Object -TypeName psobject -Property @{
+    RegistryPath = 'Registry::HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU\'
+    Entries = @('UseUpdateClassPolicySource','UseWUServer','AUOptions','AllowAutoUpdate','NoAutoUpdate')
+}
+
+#Windows Update
+$keys+= New-Object -TypeName psobject -Property @{
+    RegistryPath = 'Registry::HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate'
+    Entries = @('DeferFeatureUpdates','DeferFeatureUpdatesPeriodInDays','PauseFeatureUpdatesStartTime','DeferQualityUpdates','DeferQualityUpdatesPeriodInDays','PauseQualityUpdatesStartTime','ScheduledInstallDay','ScheduledInstallTime','ExcludeWUDriversInQualityUpdate','BranchReadinessLevel','DoNotConnectToWindowsUpdateInternetLocations','DisableWindowsUpdateAccess','SetPolicyDrivenUpdateSourceForDriverUpdates','SetPolicyDrivenUpdateSourceForQualityUpdates','SetPolicyDrivenUpdateSourceForFeatureUpdates','WUServer')
+}
+
+foreach ($key in $keys)
+{
+    try
+    {
+        $value = $null = Get-ItemProperty -Path $key.RegistryPath -ErrorAction Stop
+
+        foreach ($entry in $key.Entries)
+        {
+            if (-not [string]::IsNullOrWhiteSpace($value.$entry))
+            {
+                try
+                {
+                    Write-Host ('[{0}]: Removing conflicting value [{1}]' -f $key.RegistryPath, $entry)
+                    $null = Remove-ItemProperty -Path $key.RegistryPath -Name $entry -ErrorAction Stop
+                }
+                catch
+                {
+                    Write-Host ('[{0}]: Issue remove conflicting value [{1}]' -f $key.RegistryPath, $entry)
+                }
+            } 
+        }
+    }
+    catch
+    {
+        Write-Host (' [{0}]: Issue removing key' -f $key.RegistryPath)
+    }
+}
